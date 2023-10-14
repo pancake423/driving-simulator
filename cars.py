5862756b103087e90c97471d5aa278857c96f406
J5wingo123 <jaredegrubb@gmail.com>
2023-10-14 16:46:54 -0400

    Create cars.py
    Got a start on AbtractCar class that the player and computer cars will inherit from. Still need to figure out whether to use rect or x and y values for position, and the AbstractCar class probably needs as a move() method and a getSurface() method.

---
+++ cars.py
@@ -0,0 +1,36 @@
+import pygame
+import math
+
+class AbstractCar(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__(self)
+        
+        self.image = self.IMG
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect = self.image.getrect(center=(self.START))
+        
+        self.maxSpeed = 20
+        self.turnSpeed = 2
+        self.velocity = 0
+        self.acceleration = 2
+        self.angle = 0
+        
+    def accelerate(self):
+        self.velocity += self.acceleration
+        self.velocity = min(maxSpeed, velocity)
+    
+    def deccelerate(self):
+        self.velocity -= self.acceleration
+        
+    def turn(self, dir):
+        oldCenter = self.rect.center
+        
+        if dir.lower() == "left":
+            self.angle -= self.turnSpeed
+            self.image = pygame.transform.rotate(self.image, -self.turnSpeed)
+        else dir.lower() == "right":
+            self.angle += self.turnSpeed
+            self.image = pygame.transform.rotate(self.image, -self.turnSpeed)
+        
+        self.rect = self.image.getrect(center=oldCenter)
+        
