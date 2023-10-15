f20905bf8385c2c397a4015461f383900f0c5058
J5wingo123 <jaredegrubb@gmail.com>
2023-10-15 18:18:16 -0400

    Update cars.py
    Filled out more of BotCar class, as well as some adjustments to AbstractCar and PlayerCar. Attempted to make a little demo of the AI car following the player's mouse, but it isn't currently working.

--- cars.py
+++ cars.py
@@ -3,11 +3,11 @@ import math
 
 class AbstractCar(pygame.sprite.Sprite):
     def __init__(self):
-        super().__init__(self)
+        super().__init__()
         
         self.image = self.IMG
         self.mask = pygame.mask.from_surface(self.image)
-        self.rect = self.image.getrect(center=(self.START))
+        self.rect = self.image.get_rect(center=(self.START))
         
         self.maxSpeed = 20
         self.turnSpeed = 2
@@ -17,33 +17,34 @@ class AbstractCar(pygame.sprite.Sprite):
         
     def accelerate(self):
         self.velocity += self.acceleration
-        self.velocity = min(maxSpeed, velocity)
+        self.velocity = min(self.maxSpeed, self.velocity)
     
     def decelerate(self):
         self.velocity -= self.acceleration
         
     def turn(self, dir):
         oldCenter = self.rect.center
+        turnAmount = self.turnSpeed * self.velocity
         
         if dir.lower() == "left":
-            self.angle -= self.turnSpeed
-            self.image = pygame.transform.rotate(self.image, -self.turnSpeed)
-        else dir.lower() == "right":
-            self.angle += self.turnSpeed
-            self.image = pygame.transform.rotate(self.image, -self.turnSpeed)
+            self.angle -= turnAmount
+            self.image = pygame.transform.rotate(self.image, -turnAmount)
+        elif dir.lower() == "right":
+            self.angle += self.turnSpeed * self.velocity
+            self.image = pygame.transform.rotate(self.image, turnAmount)
         
-        self.rect = self.image.getrect(center=oldCenter)
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect = self.image.get_rect(center=oldCenter)
         
     def move(self):
-        delta_x = self.velocity * math.cos(self.angle)
-        delta_y = self.velocity * math.sin(self.angle)
+        delta_x = self.velocity * math.cos(math.radians(self.angle))
+        delta_y = self.velocity * math.sin(math.radians(self.angle))
         
-        self.x += delta_x
-        self.y += delta_y
+        self.rect.move(delta_x, delta_y)
         
 class PlayerCar(AbstractCar):
     def __init__(self):
-        super().__init__(self)
+        super().__init__()
         
     def player_input(self):
         keys = pygame.key.get_pressed()
@@ -61,16 +62,73 @@ class PlayerCar(AbstractCar):
         self.move()
             
 class BotCar(AbstractCar):
+    IMG = pygame.image.load("assets\\unicorn-car-blue.svg")
+    START = (100, 100)
+    
     def __init__(self):
-        super().__init__(self)
+        self.IMG = pygame.transform.scale(self.IMG, (200, 200)).convert_alpha()
+        super().__init__()
         self.target = (self.rect.center)
     
     #Move the bot towards the target
     def bot_move(self):
+        x, y = self.rect.center
+        targetX, targetY = self.target
+        dX, dY = targetX - x, targetY - y
+        distance = math.sqrt(dX**2 + dY**2)
+        targetAngle = math.degrees(math.atan2(dY, dX)) % 360
+        dAngle = targetAngle - self.angle
         
+        # Change angle
+        if distance < 5: # Buffer zone to prevent spinning near target       
+            if dAngle < 180:
+                self.turn("left")
+            else:
+                self.turn("right")
+                
+        # Change speed
+        if distance > 3:
+            if dAngle <= 90 or dAngle >= 270:
+                self.accelerate()
+            else:
+                self.decelerate()
+        
+    def setTarget(self, target):
+        self.target = target
+        
+    def newTarget(self):
+        x, y = self.rect.center
+        targetX, targetY = self.target
+        dX, dY = targetX - x, targetY - y
+        distance = math.sqrt(dX**2 + dY**2)
+        
+        return distance < 4
     
     def update(self):
-        bot_move()
+        self.bot_move()
         self.move()
     
+if __name__ == "__main__":
+    screen = pygame.display.set_mode((1080, 720))
+    pygame.display.set_caption("AI Car demo")
     
+    myGroup = pygame.sprite.Group()
+    myCar = BotCar()
+    myGroup.add(myCar)
+    
+    clock = pygame.time.Clock()
+    run = True
+    while run:
+        clock.tick(60)
+        screen.fill((50, 200, 50))
+        myCar.setTarget(pygame.mouse.get_pos())
+        myGroup.update()
+        myGroup.draw(screen)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                
+        pygame.display.flip()
+    
+    pygame.quit()
