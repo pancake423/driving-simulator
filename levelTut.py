777e10c79fe998035bd077e2c7af6eb898d8973e
SmoCloud <psn.power@yahoo.com>
2023-11-05 13:10:47 -0500

    Adjustments
    I adjusted some of the UI so that the road would display more centered. I also began on the format for the tutorial. There were some changes I made to the Level4.py that I didn't commit here, but it was the only way to get them to work in the match-case I made for the UI (I'll talk about it at meeting later today).

--- levelTut.py
+++ levelTut.py
@@ -11,35 +11,32 @@ def level_Tutorial(screen, screen_width, screen_height):
     stopSign= pygame.image.load('assets/NewTempStop.png')
     stopSign = pygame.transform.smoothscale(stopSign,(60, 60)).convert_alpha()
 
+    # Coordinates for the intersection
+    x = screen_width / 2
+    y = screen_height / 2
+
     # Define coordinates for the horizontal roads
     fh_start_x = 0
-    fh_end_x = 432.5
-    fh_y = 360
+    fh_end_x = screen_width / 2 - 107.5
 
-    sh_start_x = 647.5
-    sh_end_x = 1080
-    sh_y = 360
+    sh_start_x = screen_width / 2 + 107.5
+    sh_end_x = screen_width
 
     # Coordinates for vertical roads
-    v_st_1 = 467.5
-    v_end_1 = 720
-    x1 = 540
+    v_st_1 = screen_height / 2 + 107.5
+    v_end_1 = screen_height
     v_st_2 = 0
-    v_end_2 = 252.5
-    x2 = 540
+    v_end_2 = screen_height / 2 - 107.5
 
-    # Coordinates for the intersection
-    inter_x = screen_width // 2
-    inter_y = screen_height // 2
 
     # Call functions to add roads, intersections, etc
-    level.add_horizontal_road(fh_start_x, fh_end_x, fh_y)
-    level.add_horizontal_road(sh_start_x, sh_end_x, sh_y)
+    level.add_horizontal_road(fh_start_x, fh_end_x, y)
+    level.add_horizontal_road(sh_start_x, sh_end_x, y)
 
     # Add vertical roads and intersection as needed
-    level.add_vertical_road(x1, v_st_1, v_end_1)
-    level.add_vertical_road(x2, v_st_2, v_end_2)
-    level.add_intersection(inter_x, inter_y)
+    #level.add_vertical_road(x1, v_st_1, v_end_1)
+    level.add_vertical_road(x, v_st_2, v_end_2)
+    level.add_intersection(x, y)
 
     level.add_random_decorations(30)
 
