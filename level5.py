1e66185a93a5092bbd6acac626292ab46c5c14cb
jkiopl <bradyparker24@gmail.com>
2023-11-12 20:09:08 -0500

    Update level 5
    Updated level 5 to be tis own class with an update method. Also implemented new 4-lane divided highway level object. Created pass scenario, but still working on getting all the failstates to work properly.

--- level5.py
+++ level5.py
@@ -3,82 +3,99 @@ from random import randint
 from levels import Level
 from cars import *
 
-def levelFive(screen, screen_width, screen_height):
+class LevelFive(Level):
     level_title = "Urge to Merge"
     level_overview = "You start this level at the beginning of a highway on-ramp. While going down the ramp, match speed with highway traffic and safely merge. Avoid stopping on the on-ramp"
-
-    #Level Elements
-    level = Level(screen_width, screen_height)
-    level.add_horizontal_road(0,screen_width,screen_height/3)
-    level.add_diagonal_road(0,screen_height,screen_width/2,screen_height/3)
-    level.add_random_decorations(20)
     
-    #Player
-    player = pygame.sprite.GroupSingle()
-    playerCar = PlayerCar((30,screen_height-30),)
-    player.add(playerCar)
-    
-    #Bots
-    bots = pygame.sprite.Group()
-    botCar1 = BotCar((20,screen_height/3 + 50))
-    botCar2 = BotCar((20,screen_height/4 + 20))
-    bots.add(botCar1)
-    bots.add(botCar2)
-    botList = [botCar1, botCar2]
-    botCar1.setTarget(((screen_width + 100,screen_height/3 + 50),False))
-    botCar2.setTarget(((screen_width + 100,screen_height/4 + 20),False))
-    
-    #Collisions
-    playerCar.setCollide([bots])
-    botCar1.setCollide([player])
-    botCar2.setCollide([player])
-    
-    clock = pygame.time.Clock()
-    last_spawned = pygame.time.get_ticks() #initializing this for later
-    run = True
-    
-    while run:
-        clock.tick(60)
+    def __init__(self,screen, screen_width, screen_height):
+        super().__init__(screen_width,screen_height)
         
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
+        #Level Elements
+        ramp_length = 750 #diagonal length of on-ramp (in pixels)
+        self.add_4_lane_divided(0,screen_height/3,screen_width,screen_height/3)
+        self.add_diagonal_road(0,screen_height,(ramp_length*math.cos(math.radians(45))),screen_height-(ramp_length*math.sin(math.radians(45))))
+        self.add_random_decorations(20)
+        
+        #Player
+        self.player = pygame.sprite.GroupSingle()
+        self.playerCar = PlayerCar((150,screen_height-60),315)
+        self.player.add(self.playerCar)
+        
+        #Bots
+        self.bots = pygame.sprite.Group()
+        self.botCar1 = BotCar((20,screen_height/3 + 90))
+        self.botCar2 = BotCar((20,screen_height/3 + 180))
+        self.bots.add(self.botCar1)
+        self.bots.add(self.botCar2)
+        self.botList = [self.botCar1, self.botCar2]
+        self.botCar1.setTarget(((screen_width + 100,screen_height/3 + 100),False))
+        self.botCar2.setTarget(((screen_width + 100,screen_height/3 + 180),False))
+        self.last_spawned = pygame.time.get_ticks() #initializing this for later
+        
+        #Collisions
+        self.playerCar.setCollide([self.bots])
+        self.botCar1.setCollide([self.player])
+        self.botCar2.setCollide([self.player])
+    
+    def update(self):
+        self.draw(screen, 0, 0)
+        self.bots.draw(screen)
+        self.bots.update()
+        self.player.draw(screen)
+        self.player.update()
 
-                
-        #If there's less than 5 bot cars, create a new one
-        if len(bots.sprites()) < 5:
-            #wait 1 second between spawning cars so they do't spawn on top of each other
-            if (pygame.time.get_ticks() - last_spawned) > 1000:
-                #Divides the cars between both lanes of the road
+        #Creates Bot Traffic
+        self.botCount = 10
+        #If there's less than botCount bot cars, create a new one
+        if len(self.bots.sprites()) < self.botCount:
+            #wait 0.5 seconds between spawning cars so they don't spawn on top of each other
+            if (pygame.time.get_ticks() - self.last_spawned) > 500:
+                #Divides traffic between both sides of the divided highway
                 if randint(0,1) == 1:
-                    botCarNum = BotCar((-50,screen_height/3 + 50))
-                    botCarNum.setTarget(((screen_width + 100,screen_height/3 + 50),False))
+                    #Divides traffic between both lanes
+                    if randint(0,1) == 1:
+                        self.botCar = BotCar((-50,screen_height/3 + 90))
+                        self.botCar.setTarget(((screen_width + 100,screen_height/3 + 90),False))
+                    else:
+                        self.botCar = BotCar((-50,screen_height/3 + 180))
+                        self.botCar.setTarget(((screen_width + 100,screen_height/3 + 180),False))
                 else:
-                    botCarNum = BotCar((-50,screen_height/4 + 20))
-                    botCarNum.setTarget(((screen_width + 100,screen_height/4 + 20),False))
+                    if randint(0,1) == 1:
+                        self.botCar = BotCar((screen_width + 50,screen_height/4),180)
+                        self.botCar.setTarget(((-200,screen_height/4),False))
+                    else:
+                        self.botCar = BotCar((screen_width + 50,screen_height/4 - 90),180)
+                        self.botCar.setTarget(((-200,screen_height/4 - 90),False))
                 
-                last_spawned = pygame.time.get_ticks()
+                self.last_spawned = pygame.time.get_ticks()
                 
-                bots.add(botCarNum)
-                botList.append(botCarNum)
-                botCarNum.setCollide([player])
-            
+                self.bots.add(self.botCar)
+                self.botList.append(self.botCar)
+                self.botCar.setCollide([self.player])
+                
         else:    
-            #Deletes bots once they get to the right and recreates them on the left
-            for botCar in botList:
-                if botCar.rect.x > screen_width + 50:
-                   botList.remove(botCar)
+            #Deletes bots once they go offscreen
+            for botCar in self.botList:
+                if botCar.rect.x > screen_width + 100 or botCar.rect.x < -100:
+                   self.botList.remove(botCar)
                    botCar.kill()
+                   
+        #Road Rules
+        timer = pygame.time.get_ticks() #initializing for later
+        roads = self.get_targets(self.playerCar)#Checks if player is on the road
+        if self.playerCar.isStopped():
+            return "Fail"
+        
+        #if player is offroad or between roads for more than a second, fail
+        if len(roads) != 1:
+            if pygame.time.get_ticks() - timer > 1000:
+                return "Fail"
+            timer = pygame.time.get_ticks()
             
-        screen.fill(level.BG_COLOR)
-        level.draw(screen, 0, 0)
-        
-        bots.draw(screen)
-        bots.update()
-        player.draw(screen)
-        player.update()
-        
-        pygame.display.flip()
+        if self.playerCar.rect.x > screen_width + 50:
+            return "Pass"
+        else:
+            return "NA"
         
 if __name__ == "__main__":
     pygame.init()
@@ -86,6 +103,22 @@ if __name__ == "__main__":
     screen_height = 1080
     screen = pygame.display.set_mode((screen_width, screen_height), pygame.FULLSCREEN)
     
-    levelFive(screen, screen_width, screen_height)
+    clock = pygame.time.Clock()
+    level = LevelFive(screen,screen_width,screen_height)
+    run = True
+    
+    while run:
+        clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                
+        status = level.update()
+        if status == "Pass" or status == "Fail":
+            run = False
+            print(status)
+            
+        pygame.display.flip()
+    
     pygame.quit()
     
