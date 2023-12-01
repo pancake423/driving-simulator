480a016cca351b1708e0d1668915eb66c06f734b
Ethan Quezada <117426239+Ethan-J-Quezada@users.noreply.github.com>
2023-12-01 18:30:46 -0500

    Level 1 update
    things added [temporary ai cars, pass/fail parameters, user car is now movable]

--- Level1.py
+++ Level1.py
@@ -1,81 +1,186 @@
 import pygame
+import math
+from cars import PlayerCar, BotCar
 from levels import Level
-from cars import PlayerCar
 
-def level_One(screen, screen_width, screen_height):
-    #pygame.init()
-
-    # Create a level instance
-
-    level = Level(screen_width, screen_height)
-
-    # Define coordinates for the horizontal roads
-    fh_start_x = 0  
-    fh_end_x = 850
-    fh_y = 540
-
-    sh_start_x = 1068
-    sh_end_x = 2160
-    sh_y = 540
-
-    #Coordinates for vertical roads
-    #R1 bottom road
-    v_st_1 = 1485
-    v_end_1 = 648 #650
-    x1 = 960
+class levelone(Level):
     
-    #top road
-    v_st_2 = 0
-    v_end_2 = 432 #430
-    x2 = 960
+    def __init__(self, screen, screen_width, screen_height):
+        super().__init__(screen_width, screen_height)
+        #Declarations
+        levelNum = "Level 1"
+        
+        #Create coordinates for the horizontal road
+          # Define coordinates for the horizontal roads
+        fh_start_x = 0  
+        fh_end_x = 850
+        fh_y = 540
 
-    # Coordinates for the intersection
-    inter_x = screen_width // 2
-    inter_y = screen_height // 2
+        sh_start_x = 1068
+        sh_end_x = 2160
+        sh_y = 540
 
-    player = pygame.sprite.GroupSingle()
-    playerCar = PlayerCar((200,590),)
-    player.add(playerCar)
+        #Coordinates for vertical roads
+        #R1 bottom road
+        v_st_1 = 1485
+        v_end_1 = 648 #650
+        x1 = 960
+        
+        #top road
+        v_st_2 = 0
+        v_end_2 = 432 #430
+        x2 = 960
 
-    #add traffic lights
-    level.add_4_way_light(inter_x, inter_y)
+        # Coordinates for the intersection
+        inter_x = screen_width // 2
+        inter_y = screen_height // 2
 
-    # Call functions to add roads, intersections, etc
-    level.add_horizontal_road(fh_start_x, fh_end_x, fh_y)
-    level.add_horizontal_road(sh_start_x, sh_end_x, sh_y)
-    # Add vertical roads and intersection as needed
-    level.add_vertical_road(x1, v_st_1, v_end_1)
-    level.add_vertical_road(x2, v_st_2, v_end_2)
-    level.add_intersection(inter_x, inter_y)
-    
 
-    
+        #font type
+        fontType = "fonts/Get Now.ttf"
+        levelFont = pygame.font.Font(fontType, 40)
+        self.level_surf = levelFont.render(levelNum, True, "red")
+        self.level_rect = self.level_surf.get_rect(topleft=(screen_width - 150, 20))
+        
+        self.topLaneY = screen_height / 2 - 110
+        self.botLaneY = screen_height / 2 + 10
+        
+        self.playerCar = PlayerCar((80, 600))
+        self.playerGroup = pygame.sprite.GroupSingle()
+        self.playerGroup.add(self.playerCar)
+        
+        self.botCar = BotCar((screen_width, self.topLaneY + 50), 180)
+        self.botCar2 = BotCar((x1 - 50, 50), 90)
+        self.botCar3 = BotCar((x2 + 50, screen_height - 50), 270)
+        
+        self.botGroup = pygame.sprite.Group()
+        
+        self.botGroup.add(self.botCar)
+        self.botGroup.add(self.botCar2)
+        self.botGroup.add(self.botCar3)
+        
+        self.botCar.setTarget(((1150, self.topLaneY + 50), True))
+        self.botCar2.setTarget(((900, self.topLaneY - 60), True))
+        self.botCar3.setTarget(((1000, self.topLaneY + 270), True))
+        
+        self.playerCar.setCollide([self.botGroup])
+        self.botCar.setCollide([self.playerGroup])
+        self.botCar2.setCollide([self.playerGroup])
+        self.botCar3.setCollide([self.playerGroup])
 
-    level.add_random_decorations(30)
+        #displays level 6 on screen
+        pygame.display.set_caption(levelNum)
+              
+        
+        #call functions to add roads, intersections, etc
+        self.add_horizontal_road(fh_start_x, fh_end_x, fh_y)
+        self.add_horizontal_road(sh_start_x, sh_end_x, sh_y)
+        self.add_vertical_road(x1, v_st_1, v_end_1)
+        self.add_vertical_road(x2, v_st_2, v_end_2)
+        self.add_intersection(inter_x, inter_y)
+        self.add_random_decorations(50)
+        
+        
+        #Adds stop sign
+        self.add_4_way_light(inter_x, inter_y)
+        
+        #variables for timer
+        self.carsGone = 0
+        self.timer = pygame.time.get_ticks()
 
-    level.draw(screen, 0, 0)
+    def update(self,screen):
+        # Clear the screen
+        screen.fill(self.BG_COLOR) 
 
-    player.draw(screen)
-    player.update
+        # Draw the level
+        self.draw(screen, 0, 0)
+        
 
-    return level
+        
+        
+        if self.carsGone == 0 and self.botCar.newTarget() and (pygame.time.get_ticks() - self.timer()) / 1000 > 2:
+            self.timer = pygame.time.get_ticks()
+        if (pygame.time.get_ticks() - self.timer) / 1000 > 2:
+            self.timer = pygame.time.get_ticks()
+            if self.carsGone == 0:
+                self.botCar3.setTarget(((1000, -200), True))
+            elif self.carsGone == 1:   
+                self.botCar2.setTarget(((900, self.height + 200), True))
+            elif self.carsGone == 2:
+                self.botCar.setTarget(((-200, self.topLaneY + 50), True))
+            self.carsGone += 1
 
-# Entry point for your program
+        
+            
+            
+
+        playerPos, botPos = self.playerCar.getPos(), self.botCar.getPos()
+        playerX, playerY = playerPos
+        botX, botY = botPos
+        
+        self.playerGroup.update()
+        self.botGroup.update()
+        self.playerGroup.draw(screen)
+        self.botGroup.draw(screen)
+
+#Road Rules
+        roads = self.get_targets(self.playerCar)#Checks if player is on the road
+        
+        timer = pygame.time.get_ticks()
+        if self.playerCar.isStopped():
+            #waits 3 seconds before failing so player can see the explosion
+            if (pygame.time.get_ticks() - timer) > 3000:
+                return "Fail"
+        
+        #if player is offroad or between roads for more than a second, fail
+        if len(roads) != 1:
+            if bool(self.playerCar.isOffRoad()) == False:
+                self.playerCar.offRoad = True
+                self.offRoadTimer = pygame.time.get_ticks()
+                
+            else:
+                if pygame.time.get_ticks() - self.offRoadTimer >= 1000:
+                    return "Fail"
+            
+        else:
+            self.playerCar.offRoad = False
+            
+        #if player goes past the median, fail
+        if self.playerCar.rect.y < (self.height/3):
+            return "Fail"
+            
+        #If player successfully merges and continues to the right, pass
+        if self.playerCar.rect.x > self.width + 50:
+            return "Pass"
+        
+        else:
+            return "NA"
+
 if __name__ == "__main__":
+    # Initialize pygame
     pygame.init()
-    pygame.mixer.init()
+    # Set screen
     screen_width = 1920
     screen_height = 1080
-
-    screen = pygame.display.set_mode((screen_width, screen_height))
-
-    level = level_One(screen, screen_width, screen_height)
-    while True:
+    screen = pygame.display.set_mode((screen_width, screen_height),pygame.FULLSCREEN | pygame.SCALED)
+    
+    #calls the level_Three() Funcntion
+    clock = pygame.time.Clock()
+    run = True
+    myLevel = levelone(screen, screen_width, screen_height)
+    while run:
+        clock.tick(60)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
-                exit()
+                run = False
+        status = myLevel.update(screen)
         
-        
+        if status == "Pass" or status == "Fail":
+            run = False
+            print(status)
+                
         pygame.display.flip()
-        pass  # The game loop will keep running until the game is closed
+    
+    pygame.quit()
+        
+
