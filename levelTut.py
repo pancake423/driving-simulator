eca3494445c25dcc68804b860d707cf61fd981c4
SmoCloud <psn.power@yahoo.com>
2023-12-03 21:44:14 -0500

    Removed old code
    Old code from when level was a function (commented out) was deleted to reduce file size.

--- levelTut.py
+++ levelTut.py
@@ -305,39 +305,3 @@ if __name__ == "__main__":
         pygame.display.flip()
     
     pygame.quit()
-
-
-
-    """
-    #pygame.init()
-
-    # Create a level instance
-
-    level = Level(screen_width, screen_height)
-    
-    #stopSign= pygame.image.load('assets/NewTempStop.png')
-    #stopSign = pygame.transform.smoothscale(stopSign,(60, 60)).convert_alpha()
-
-    # Coordinates for the intersection
-    x = screen_width / 2
-    y = screen_height / 2
-
-    # Define coordinates for the horizontal roads
-    fh_end_x = screen_width / 2 - 107.5
-
-    sh_start_x = screen_width / 2 + 107.5
-
-    # Coordinates for vertical roads
-    v_end_2 = screen_height / 2 - 107.5
-
-    # Call functions to add roads, intersections, etc
-    level.add_horizontal_road(0, fh_end_x, y)
-    level.add_horizontal_road(sh_start_x, screen_width, y)
-
-    # Add vertical roads and intersection as needed
-    level.add_vertical_road(x, 0, v_end_2)
-    level.add_intersection(x, y)
-
-    level.add_random_decorations(30)
-    """
-    #return level
